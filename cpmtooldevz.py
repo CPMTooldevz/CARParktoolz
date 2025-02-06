54b01e908331ceb1a3d6e4c7203a6ca6e3811a5b
CPMTooldevz <telmnanotheremail@gmail.com>
2025-02-07 04:41:46 +0800

    Update and rename Test.py to cpmtooldevz.py

--- cpmtooldevz.py
+++ cpmtooldevz.py
@@ -2,7 +2,7 @@ import requests
 from time import sleep
 BASE_URL: str = "https://telmunntest.squareweb.app/api"
 
-class telmnnuker:
+class CPMTooldevz:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
