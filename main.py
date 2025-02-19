292e17b1eb3256d5af16d5572dbb1d52e41242eb
CPMTooldevz <telmnanotheremail@gmail.com>
2025-02-20 01:05:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -529,7 +529,7 @@ if __name__ == "__main__":
                     continue
             elif service == 24: # Unlocking Wheels
                console.print("[bold cyan][%] Unlocking Wheels[/bold cyan]: ", end=None)
-              if cpm.unlock_wheels():
+               if cpm.unlock_wheels():
                     console.print("[bold green]SUCCESSFUL.[/bold green]")
                     console.print("==================================")
                     answ = Prompt.ask("[bold cyan][?] Do You want to Exit ?[/bold cyan]", choices=["y", "n"], default="n")
