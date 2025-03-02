a74999ef1c6b48b809f4746dcd00f3e15b6f50da
CPMTooldevz <telmnanotheremail@gmail.com>
2025-03-03 02:17:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -531,9 +531,9 @@ if __name__ == "__main__":
                 console.print("[bold cyan][%] Unlocking Wheels[/bold cyan]: ", end=None)
     
                 if cpm.unlock_wheels():
-                console.print("[bold green]SUCCESSFUL.[/bold green]")
-                console.print("==================================")
-                answ = Prompt.ask("[bold cyan][?] Do You want to Exit ?[/bold cyan]", choices=["y", "n"], default="n")
+                   console.print("[bold green]SUCCESSFUL.[/bold green]")
+                   console.print("==================================")
+                   answ = Prompt.ask("[bold cyan][?] Do You want to Exit ?[/bold cyan]", choices=["y", "n"], default="n")
         
                 if answ == "y":
                 console.print("[bold yellow][!] Thank You for using our tool.[/bold yellow].")
